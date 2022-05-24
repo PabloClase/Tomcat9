608df684856a26d9388ed776664c1f15e6289bba
PabloClase <104835753+PabloClase@users.noreply.github.com>
2022-05-24 02:07:44 +0200

    Add files via upload

---
+++ Ejemplo1.java
@@ -0,0 +1,32 @@
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/Ejemplo1")
+public class Ejemplo1 extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+       
+    /**
+     *
+     * @param request
+     * @param response
+     * @throws ServletException
+     * @throws IOException
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+        out.println("<html>");
+        out.println("<body>");
+        out.println("Nombre: "+request.getParameter("nombre") );
+        out.println("</body>");
+        out.println("</html>");
+    }
+
+}
